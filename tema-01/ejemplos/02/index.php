4a9e820796269789515b66a3e4b0cc8bde7437f2
2010jmatpon <jmatpon2010@g.educaand.es>
2023-09-20 14:53:05 +0200

    segundo commit

---
+++ tema-01/ejemplos/02/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    /*
+    Ejemplo 2 - PHP
+    Viva el Cádiz
+    */
+
+    echo '<h1>';
+    echo '<hr>';
+    echo '<center>';
+    echo 'Viva el Cádiz CF';
+    echo '</center>'; 
+    echo '<hr>';
+    echo '</h1>';
+    echo 'He ganao un llavero del Cádiz en el tirapichón';
+   
+?>
