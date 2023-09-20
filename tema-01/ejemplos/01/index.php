da42db15b4a10aaa801b4e289df30dd9e6fa5847
2010jmatpon <jmatpon2010@g.educaand.es>
2023-09-20 14:39:59 +0200

    primer commit

---
+++ tema-01/ejemplos/01/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    /*
+    Ejemplo 1 - PHP
+    Hola mundo
+    */
+
+    echo '<h1>';
+    echo '<hr>';
+    echo '<center>';
+    echo 'Hola Mundo PHP';
+    echo '<BR>';
+    echo 'Soy Juan María Mateos Ponce';
+    echo '</center>'; 
+    echo '<hr>';
+    echo '</h1>';
+    echo 'DWES - Curso 23/24';
+   
+?>
