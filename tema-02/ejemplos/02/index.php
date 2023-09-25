f76b67adb4b6adf172848359a057e06b551b3627
2010jmatpon <jmatpon2010@g.educaand.es>
2023-09-25 15:01:47 +0200

    Commit

--- tema-02/ejemplos/02/index.php
+++ tema-02/ejemplos/02/index.php
@@ -1,21 +1,24 @@
+<?php
+    $usuario = "Wanillo";
+?> //no hacer esto
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejemplo 01 - Tema 2</title>
+    <title>Ejemplo 02 - Tema 2</title>
 </head>
 
 <body>
     <center>
-        <h2> Ejemplo 1 - Tema 2</h2>
+        <h2> Ejemplo 2 - Tema 2</h2>
     </center>
     <h4>
         <?php
             echo "Hola Mundo ";
             echo "<br>";
-            echo "Juan M";
+            echo "soy $usuario";
         ?>
     </h4>
 </body>
