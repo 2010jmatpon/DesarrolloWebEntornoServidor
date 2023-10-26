2c010f26fe32b996ef578601eb849f8ab6133165
2010jmatpon <115532008+2010jmatpon@users.noreply.github.com>
2023-10-26 13:41:20 +0200

    Delete tema-03/proyectos/02-articulos directory

--- tema-03/proyectos/02-articulos/nuevo.php
+++
@@ -1,9 +0,0 @@
-<?php
-#Libreria
-include 'libs/crud_funciones.php';
-
-include 'models/model.nuevo.php';
-
-#Vista
-include "views/view.nuevo.php";
-?>
--- tema-03/proyectos/02-articulos/update.php
+++
@@ -1,9 +0,0 @@
-<?php
-#Libreria
-include 'libs/crud_funciones.php';
-#Model
-include "models/model.update.php";
-
-#Vista
-include "views/view.index.php";
-?>
