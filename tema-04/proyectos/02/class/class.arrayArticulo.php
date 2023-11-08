4ee0c89f83961d6b16de6dde2eda138406c263a7
2010jmatpon <jmatpon2010@g.educaand.es>
2023-11-08 14:27:20 +0100

    Commit

--- tema-04/proyectos/02/class/class.arrayArticulo.php
+++ tema-04/proyectos/02/class/class.arrayArticulo.php
@@ -1,46 +1,139 @@
 <?php
 
-    /* 
-        class.arrayArticulos.php
-        
-        tabla de Artículos
+/* 
+    class.arrayArticulos.php
+    
+    tabla de Artículos
 
-        Es un array donde cada elemento es un objeto de la clase Articulo
-    */
+    Es un array donde cada elemento es un objeto de la clase Articulo
+*/
 
-    class ArrayArticulos {
-        private $tabla;
+class ArrayArticulos
+{
+    private $tabla;
 
-        public function __construct($tabla) {
-        
-            $this->tabla = [];
-        
-        }
+    public function __construct()
+    {
 
+        $this->tabla = [];
 
-        /**
-         * Get the value of tabla
-         */ 
-        public function getTabla()
-        {
-                return $this->tabla;
-        }
-
-        /**
-         * Set the value of tabla
-         *
-         * @return  self
-         */ 
-        public function setTabla($tabla)
-        {
-                $this->tabla = $tabla;
-
-                return $this;
-        }
-
-        public function getCategorias(){
-            
-        }
     }
+
+
+    /**
+     * Get the value of tabla
+     */
+    public function getTabla()
+    {
+        return $this->tabla;
+    }
+
+    /**
+     * Set the value of tabla
+     *
+     * @return  self
+     */
+    public function setTabla($tabla)
+    {
+        $this->tabla = $tabla;
+
+        return $this;
+    }
+
+    static public function getMarcas()
+    {
+        $marcas = [
+            'Xiaomi',
+            'Acer',
+            'Aoc',
+            'Nokia',
+            'Apple',
+            'Lenovo',
+            'IBM'
+        ];
+        asort($marcas);
+        return $marcas;
+
+    }
+
+    static public  function getCategorias()
+    {
+        $categorias = [
+            'Portátil',
+            'PC sobremesa',
+            'Componente',
+            'Pantalla',
+            'Impresora',
+            'Tablets',
+            'Móviles',
+            'Fotografía',
+            'Imagen'
+        ];
+        asort($categorias);
+        return $categorias;
+
+    }
+
+    public function getDatos()
+    {
+        #Articulo1
+        $articulo = new Articulo(
+            1,
+            'Portátil - HP 15-DB0074NS',
+            'HP 15-DB0074NS',
+            0,
+            [1, 2, 3],
+            120,
+            379
+        );
+
+        #Añadir artículo a la tabla
+        $this -> tabla[] = $articulo;
+
+        #Articulo2
+        $articulo = new Articulo(
+            2,
+            'Portátil AMD A4-9125, 8 GB RAM, 125 GB',
+            'HP 255 G7, 15.6',
+            0,
+            [2, 3, 5],
+            200,
+            20.5
+        );
+
+        #Añadir artículo a la tabla
+        $this -> tabla[] = $articulo;
+
+        #Articulo3
+        $articulo = new Articulo(
+            3,
+            'PC sobremesa - Lenovo Intel® Core™ i3-8100',
+            'ideacentre 510S-07|CB',
+            1,
+
+            50,
+            12.95
+        );
+
+        #Añadir artículo a la tabla
+        $this -> tabla[] = $articulo;
+
+    }
+
+    static public function mostrarCategorias($categorias, $categoriasArticulo) {
+        //Podemos declarar este metodo estático porque no modifica ningun atributo de la clase
+        $arrayCategorias = [];
+    
+        foreach($categoriasArticulo as $indice) {
+    
+            $arrayCategorias[] = $categorias[$indice];
+    
+        }
+    
+        asort($arrayCategorias);
+        return $arrayCategorias;
+    
+    }
+}
 
 ?>
