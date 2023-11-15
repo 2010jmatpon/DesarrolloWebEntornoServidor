7c2eade0812f3be57b5b3e4a43bb49a49ad03a04
2010jmatpon <jmatpon2010@g.educaand.es>
2023-11-15 14:52:28 +0100

    Commit

---
+++ tema-05/proyectos/01/class/class.fp.php
@@ -0,0 +1,30 @@
+<?php
+class Fp extends Conexion
+{
+    /*
+    getAlumnos()
+
+    Devuelve un objeto conjunto resultados (mysqli_result)
+    con los detalles de todos los alumnos
+    */
+
+    public function getAlumnos()
+    {
+        $sql = "SELECT 
+                    alumnos.id,
+                    concat_ws(', ', alumnos.apellidos, alumnos.nombre) alumno,
+                    alumnos.email,
+                    alumnos.telefono,
+                    alumnos.poblacion,
+                    alumnos.dni,
+                    timestampdiff(YEAR, alumnos.fechaNac,NOW() ) EDAD,
+                    cursos.nombreCorto curso
+                FROM
+                    alumnos
+                    INNER JOIN 
+                    cursos
+                    ON alumnos.id_curso = cursos.id
+                ORDER BY id";
+    }
+}
+?>
