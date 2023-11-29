73d5925fec80cfae5348f660489b37e7139942a7
2010jmatpon <jmatpon2010@g.educaand.es>
2023-11-29 13:34:12 +0100

    Commit

--- tema-05/ejemplos/07/index.php
+++ tema-05/ejemplos/07/index.php
@@ -3,7 +3,16 @@ Class Conexion{
     public function __construct(){
         try{
             $dsn ="mysql:host=" . SERVER . ";dbname=". BD;
-        } catch()
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_PERSISTENT => false,
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES ".CHARSET."COLLATE". COLECTION
+            ];
+            $this->pdo =new PDO($dsn, USER, PASS, $options);
+        } catch(PDOException $e){
+
+        }
     }
 }
 ?>
