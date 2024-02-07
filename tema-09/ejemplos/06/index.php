23f4993470f48a5f115f33f13f32b7464bd37ab3
2010jmatpon <jmatpon2010@g.educaand.es>
2024-02-07 14:57:12 +0100

    Commit

--- tema-09/ejemplos/06/index.php
+++ tema-09/ejemplos/06/index.php
@@ -24,7 +24,7 @@ $pdf->Cabecera();
 foreach($articulos as $articulo){
     $pdf->Cell(10, 7, iconv('UTF-8', 'ISO-8859-1', $articulo['id']), 'B', 0, 'R');
     $pdf->Cell(90, 7, iconv('UTF-8', 'ISO-8859-1', $articulo['Posicion']), 'B', 0, 'C');
-    $pdf->Cell(90, 7, iconv('UTF-8', 'ISO-8859-1', $articulo['Nombre']), 'B', 0, 'C');
+    $pdf->Cell(90, 7, iconv('UTF-8', 'ISO-8859-1', $articulo['Nombre']), 'B', 1, 'C');
 }
 
 $pdf->Output();
