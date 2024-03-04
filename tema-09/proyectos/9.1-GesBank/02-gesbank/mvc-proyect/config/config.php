24acacf011389336d9cc1c5f74b35b44461ad8c1
2010jmatpon <jmatpon2010@g.educaand.es>
2024-03-04 13:16:59 +0100

    Commit

---
+++ tema-09/proyectos/9.1-GesBank/02-gesbank/mvc-proyect/config/config.php
@@ -0,0 +1,15 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+define('URL', 'http://localhost/dwes/DesarrolloWebEntornoServidor/tema-09/proyectos/9.1-GesBank/02-gesbank/mvc-proyect/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'gesbank');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+
+
+?>
